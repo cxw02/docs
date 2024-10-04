c8220a9e8f2c7b41cf5ca94ccc81b86b03586cc3
cxw02 <yg02@qq.com>
2024-10-04 08:46:59 +0800

    first commit

---
+++ assets/portfolio.html-CdN4oOdt.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as t,a as o,o as r}from"./app-DqblKm8F.js";const n={};function m(a,s){return r(),t("div",null,[o(` ## 介绍
+
+这是一个档案主页的案例。
+
+要使用此布局，你应该在页面 Frontmatter 中设置 \`home: true\` 和 \`portfolio: true\`。
+
+相关配置文档请见 [档案主页](https://theme-hope.vuejs.press/zh/guide/blog/home.html#档案类型主页)。 `)])}const h=e(n,[["render",m],["__file","portfolio.html.vue"]]),p=JSON.parse('{"path":"/portfolio.html","title":"档案主页","lang":"zh-CN","frontmatter":{"home":true,"portfolio":true,"title":"档案主页","icon":"home","welcome":"👋 你好，我是","name":"CHENXW","avatar":"/assets/image/bg/none.png","bgImage":"/assets/image/bg/9.webp","titles":["数码科技爱好者","分享与热心帮助","智能家居小能手"],"footer":true,"gitInclude":[]},"headers":[],"readingTime":{"minutes":0.34,"words":101},"filePathRelative":"portfolio.md","excerpt":"<!-- ## 介绍\\n\\n这是一个档案主页的案例。\\n\\n要使用此布局，你应该在页面 Frontmatter 中设置 `home: true` 和 `portfolio: true`。\\n\\n相关配置文档请见 [档案主页](https://theme-hope.vuejs.press/zh/guide/blog/home.html#档案类型主页)。 -->\\n"}');export{h as comp,p as data};
